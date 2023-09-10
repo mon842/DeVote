13024c370f5ef80da5baca65c850266e02096865
sayak basak <91783288+mon842@users.noreply.github.com>
2023-09-10 20:20:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import { NotificationProvider } from '@web3uikit/core';
 import './App.css';
-import Voting from './components/Voting/Voting';
+import Voting from './Components/Voting/Voting';
 
 function App() {
 
